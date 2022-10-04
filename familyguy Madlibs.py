99a01676eff53680bc648c363273ac9309197af0
Thomas Getler <thomas.getler@ryerson.ca>
2022-10-04 19:19:19 -0400

    Create familyguy Madlibs.py

---
+++ familyguy Madlibs.py
@@ -0,0 +1,6 @@
+verb=input("Please Input a verb")
+celeb=input("Please Input a random celeberity")
+object2=input("Please input an object")
+madlibs= f"Lois your acting like this is the first time i did something \
+stupid remember when I got caught {verb} {celeb} in your moms {object2} ahhahah"
+print(madlibs)
